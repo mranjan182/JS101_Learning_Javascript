b332287c059507691c0015737e4f260079747b01
mranjan182 <mukesh.bcst@gmail.com>
2023-08-26 01:38:48 +0530

    intro to javascript day 1 problem solution

---
+++ problem2.js
@@ -0,0 +1,15 @@
+// Define a variable called name with your Name as the assigned value
+// Print the value stored in the variable name
+// Change the variable to store your father's name
+// Print the value stored in the variable name
+// Change the variable again to store your mother's name.
+// Print the value stored in the variable name
+
+let name="Mukesh Ranjan"
+console.log(name);
+
+name="Ramesh Prasad Ray"
+console.log(name);
+
+name="Manorma Devi";
+console.log(name)
---
+++ problem4.js
@@ -0,0 +1,62 @@
+// Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+// Print the report card of the student (You can make it look nice by using some keyboard symbols )
+// Explore ASCII ART (https://en.wikipedia.org/wiki/ASCII_art (Links to an external site.)) or Text Art (https://fsymbols.com/text-art/ (Links to an external site.)) for some inspiration
+let symbol="------------------------------------------------------------------------"
+console.log(symbol)
+let name="|Name"+"  "+"|"
+let school='School'+"          "+'|';
+let grade='Grade|'
+let section="Section|"
+let subject1="Subject1|";
+let subject2="Subject2|";
+let subject3="Subject3|";
+
+console.log(name,school,grade,section,subject1,subject2,subject3)
+console.log(symbol)
+
+ name="|Rajesh|"
+ school='St. Paul Academy|'
+ grade='A1'+"   "+'|'
+ section='B'+"      "+'|';
+ subject1=90+"      "+'|';
+ subject2=85+"      "+'|';
+ subject3=90+"      "+'|';
+console.log(name,school,grade,section,subject1,subject2,subject3)
+
+console.log(symbol)
+
+ name="|Ritesh|"
+ school='St. Paul Academy|'
+ grade='A2'+"   "+'|'
+ section='B'+"      "+'|';
+ subject1=80+"      "+'|';
+ subject2=75+"      "+'|';
+ subject3=70+"      "+'|';
+
+console.log(name,school,grade,section,subject1,subject2,subject3)
+
+console.log(symbol)
+
+ name="|Rohan"+" "+"|";
+ school='St. Paul Academy|'
+ grade='B1'+"   "+'|'
+ section='B'+"      "+'|';
+ subject1=70+"      "+'|';
+ subject2=75+"      "+'|';
+ subject3=70+"      "+'|';
+
+console.log(name,school,grade,section,subject1,subject2,subject3)
+
+console.log(symbol)
+
+name="|Sohan"+" "+"|";
+ school='St. Paul Academy|'
+ grade='B2'+"   "+'|'
+ section='B'+"      "+'|';
+ subject1=60+"      "+'|';
+ subject2=75+"      "+'|';
+ subject3=60+"      "+'|';
+
+console.log(name,school,grade,section,subject1,subject2,subject3)
+
+console.log(symbol)
